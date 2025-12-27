4fa0dfc172191c50900d4c481cb273b912b0b97a
karol <karol.zamrowski@edu.uekat.pl>
2025-12-27 14:45:02 +0100

    Dodano zad_6.py

---
+++ zad_6.py
@@ -0,0 +1,11 @@
+# Kolejnosc zdarzeń: łączenie list, usunięcie duplikatów,potęga 3
+def fpolacz_i_podnies_do_szescianu(lista1, lista2):
+    polaczone = lista1 + lista2
+    bez_duplikatow = list(set(polaczone))
+
+    wynik = []
+    for x in bez_duplikatow:
+        wynik.append(x ** 3)
+    return wynik
+
+print(fpolacz_i_podnies_do_szescianu([ 2, 3, 4], [5, 6, 4]))
