1f03fe7757cd16b1a0a56e00c2fe85e1e9ac566d
karol <karol.zamrowski@edu.uekat.pl>
2025-12-27 13:55:01 +0100

    Dodano zad_3.py

---
+++ zad_3.py
@@ -0,0 +1,9 @@
+def fsprparzystosc(lb) -> bool:
+    return (lb % 2) == 0
+
+Lb = input("Podaj liczbę: ")
+
+if not fsprparzystosc(int(Lb)):
+    print(str(Lb) + ' liczba nieparzysta')
+else:
+    print(str(Lb) + ' liczba parzysta')
